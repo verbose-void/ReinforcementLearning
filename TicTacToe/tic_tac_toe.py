2e6496eba97cce3f4722ef71462d39e7da77d3d0
McCrearyD <dyllanmccreary@gmail.com>
2018-10-05 05:53:27 -0700

    + handle draw cases

--- TicTacToe/tic_tac_toe.py
+++ TicTacToe/tic_tac_toe.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class Environment:
     def __init__(self, size=3):
         # Generate board in a matrix
@@ -42,13 +45,30 @@ class Environment:
                     winP = False
 
         self.winner = char if winN or winP else None
+
+        # Check for draw
+        if self.winner is None:
+            draw = True
+            for row in self.tiles:
+                for tile in row:
+                    if tile is None:
+                        draw = False
+                        break
+            self.winner = "draw" if draw else self.winner
+
         return self.winner
 
 
 env = Environment()
 env.update_and_get_winner(0, 0, "X")
-env.update_and_get_winner(1, 1, "X")
-env.update_and_get_winner(2, 2, "X")
+env.update_and_get_winner(1, 1, "O")
+env.update_and_get_winner(2, 2, "O")
+env.update_and_get_winner(0, 1, "X")
+env.update_and_get_winner(1, 2, "j")
+env.update_and_get_winner(2, 0, "O")
+env.update_and_get_winner(0, 2, "f")
+env.update_and_get_winner(1, 0, "g")
+env.update_and_get_winner(2, 1, "X")
 env.draw()
 print(env.winner)
 # env.draw()
