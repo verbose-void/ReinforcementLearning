2e0ab74dfd12461496e81a172779089f98d9aacc
McCrearyD <dyllanmccreary@gmail.com>
2018-10-07 19:18:21 -0700

    + human player

---
+++ TicTacToe/human.py
@@ -0,0 +1,28 @@
+from environment import Environment
+
+
+class Human:
+    def __init__(self):
+        pass
+
+    def set_symbol(self, sym):
+        self.sym = sym
+
+    def take_action(self, env):
+        while True:
+            move = raw_input(
+                "Enter Coordinates X,Y for your next move (ie: \"0,0\")")
+            x, y = move.split(",")
+            x = int(x)
+            y = int(y)
+            if env.is_empty(x, y):
+                env.tiles[y, x] = self.sym
+                break
+            else:
+                print "Invalid Move"
+
+    def update(self, env):
+        pass
+
+    def update_state_history(self, s):
+        pass
